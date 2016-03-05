5ac8b2f745b63e4530a887d0689c6e0b684f86cb
Modestas <mode0118@gmail.com>
2016-03-06 01:04:58 +0200

    message model and query

---
+++ app/models/MessageModel.java
@@ -0,0 +1,53 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class MessageModel {
+    private int id;
+    private LocalDateTime time;
+    private String username;
+    private String message;
+
+    public MessageModel() {
+
+    }
+
+    public MessageModel(int id, LocalDateTime time, String username, String message) {
+        this.id = id;
+        this.time = time;
+        this.username = username;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ app/queries/ChatQuery.java
@@ -0,0 +1,45 @@
+package queries;
+
+import com.google.inject.Singleton;
+import models.MessageModel;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+@Singleton
+public class ChatQuery {
+    private LinkedList<MessageModel> messages = new LinkedList<>();
+
+    /**
+     * Get all messages ordered from oldest to newest
+     * @return messages
+     */
+    public LinkedList<MessageModel> getMessages() {
+        return messages;
+    }
+
+    /**
+     * Get messages with higher id than provided
+     * @param id from which id to show messages (non inclusive)
+     * @return messages
+     */
+    public LinkedList<MessageModel> getMessagesAfter(int id) {
+        LinkedList<MessageModel> after = new LinkedList<>();
+        for(MessageModel message : messages) {
+            if(message.getId() > id) {
+                after.add(message);
+            }
+        }
+        return after;
+    }
+
+    /**
+     * Add new message to chat
+     * @param username who posted it
+     * @param message what posted
+     */
+    public void addMessage(String username, String message) {
+        MessageModel model = new MessageModel(messages.size(), LocalDateTime.now(), username, message);
+        messages.add(model);
+    }
+}
