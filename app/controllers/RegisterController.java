1b59b37a8874f4bab392af39620e8b6f62a21c07
Modestas <mode0118@gmail.com>
2016-03-08 23:02:49 +0200

    register form validation

--- app/controllers/RegisterController.java
+++ app/controllers/RegisterController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.google.inject.Inject;
 import models.RegisterModel;
+import org.h2.engine.Session;
 import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Result;
@@ -24,11 +25,47 @@ public class RegisterController extends BaseController {
     }
 
     public Result submit() {
+        if(SessionUtils.isLoggedIn()) {
+            return redirect(controllers.routes.ChatController.chat());
+        }
+
+        RegisterModel model;
+        Form<RegisterModel> form;
+
+        try {
+            form = getForm().bindFromRequest();
+            model = form.get();
+        } catch (Exception ex) {
+            return error("", "Nepavyko užregistruoti!");
+        }
+
+        String username = model.getUsername();
+        String password = model.getPassword();
+        String passwordAgain = model.getPasswordAgain();
+
+        if(username == null || !username.matches("^[a-zA-Z0-9._-]{3,16}$")) {
+            return error(username, "Prisijungimo vardas turi būti nuo 3 iki 16 simbolių ilgio ir sudarytas iš raidžių, skaitmenų arba šių simbolių: . _ -");
+        }
+
+        if(password == null || !password.matches("^(?=.*\\d)(?=.*[a-zA-Z]).{6,}$")) {
+            return error(username, "Slaptažodis turi būti sudarytas iš bent vienos raidės ir bent vieno skaičiaus ir turi būti bent 6 simbolių ilgio.");
+        }
+
+        if(!password.equals(passwordAgain)) {
+            return error(username, "Slaptažodžiai nesutampa");
+        }
 
         return ok();
     }
 
     private Form<RegisterModel> getForm() {
         return formFactory.form(RegisterModel.class);
+    }
+
+    private Result error(String username, String error) {
+        RegisterModel model = new RegisterModel(username, error);
+        Form<RegisterModel> form = getForm().fill(model);
+
+        return ok(register.render(form));
     }
 }
