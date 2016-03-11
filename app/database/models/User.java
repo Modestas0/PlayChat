87d75d25d42d798001d4e793d7a9f55835d04dff
Modestas <mode0118@gmail.com>
2016-03-11 20:45:29 +0200

    User model

--- app/database/models/User.java
+++ app/database/models/User.java
@@ -1,4 +1,43 @@
 package database.models;
 
+import java.time.LocalDateTime;
+
 public class User {
+    private String username;
+    private String passwordHash;
+    private LocalDateTime lastActive;
+
+    public User() {
+
+    }
+
+    public User(String username, String passwordHash, LocalDateTime lastActive) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.lastActive = lastActive;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public LocalDateTime getLastActive() {
+        return lastActive;
+    }
+
+    public void setLastActive(LocalDateTime lastActive) {
+        this.lastActive = lastActive;
+    }
 }
